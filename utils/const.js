b36e0f1cb3f53b559cd298f60e49fc23772689a2
MuratFaizulla <muratf345@gmail.com>
2025-04-27 21:13:57 +0500

    first commit

---
+++ utils/const.js
@@ -0,0 +1,10 @@
+const gestureDescriptions = {
+    A: "A — Сәлем әлем",
+    B: "B — Бәрі жақсы",
+    C: "C — Саған сәттілік",
+    D: "D — Достық",
+    E: "E — Еркіндік",
+    // Добавляй дальше по алфавиту
+  };
+  
+  export default gestureDescriptions;  
